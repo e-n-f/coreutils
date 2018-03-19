434dc3e4fd95b85647ceefaebfa215db1896ec13
Eric Fischer <enf@pobox.com>
2018-03-19 12:33:27 -0700

    doc: correct description of cut -nb and of tr on binary files

--- src/sort.c
+++ src/sort.c
@@ -12,7 +12,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
    Written December 1988 by Mike Haertel.
    The author may be reached (Email) at the address mike@gnu.ai.mit.edu,
